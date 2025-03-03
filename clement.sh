0534d5872d25545275dfbcd3ebf2c40a4a5cdc33
Michael Mendy <montana@linux.com>
2025-03-03 05:49:42 -0800

    clement quotes

---
+++ clement.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "Defending unpopular positions is what lawyers do."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "Justice Antonin Scalia fundamentally changed the way the Supreme Court interpreted both statutes and the Constitution. In both contexts, his focus on text and its original public meaning often translated into more limited criminal prohibitions and broader constitutional protections for defendants."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "Very proud of this moment that I'm going to be taking on my first job as a head coach. Yeah, so, of course, some apprehension but not in a nervous kind of way, more in a really-excited-to-get-started way."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "This is a different game against a good team with very high qualities & we’re going to have to play at our very best."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "We totally deserved that victory. It was my job to get some confidence back into the team and I’m pleased with the players."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "We knew the Hull result, it gave us a lift, and it was about taking the opportunity and not wasting it."
+for i in {1..5}; do
+    printf "."
+    sleep 1
+done
+echo ""
+
+echo "Having a squad of Chelsea's size is unsustainable."
